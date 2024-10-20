bd14abc54a2bf27cbcf44fa71f3fdf7536e04c8b
CAINIAOJL <1625602465@qq.com>
2024-10-20 23:20:19 +0800

    lesson2 and lesson3

---
+++ Ebpf/lesson3/fentry-link.bpf.c
@@ -0,0 +1,26 @@
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+//wsl2 内核为5.15.153.1 fentry fexit 最低内核支持位5.5
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
+SEC("fentry/do_unlinkat")
+int BPF_PROG(do_unlinkat, int dfd, struct filename *name)
+{
+    pid_t pid;
+
+    pid = bpf_get_current_pid_tgid() >> 32;
+    bpf_printk("fentry: pid = %d, filename = %s\n", pid, name->name);
+    return 0;
+}
+
+SEC("fexit/do_unlinkat")
+int BPF_PROG(do_unlinkat_exit, int dfd, struct filename *name, long ret)
+{
+    pid_t pid;
+
+    pid = bpf_get_current_pid_tgid() >> 32;
+    bpf_printk("fexit: pid = %d, filename = %s, ret = %ld\n", pid, name->name, ret);
+    return 0;
+}
