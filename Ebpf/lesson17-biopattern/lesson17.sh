1bdba4eff4103d07ef599b34f810e4349841ba9f
CAINIAOJL <1625602465@qq.com>
2024-11-20 18:22:39 +0800

    测试完成,自己写的有些问题，需要查查

--- Ebpf/lesson17-biopattern/lesson17.sh
+++ Ebpf/lesson17-biopattern/lesson17.sh
@@ -3,4 +3,4 @@
 #clang -O2 -g -target bpf -c (-D__TARGET_ARCH_X86_64) xxxx.c -o xxxx.o
 clang -O2 -g -target bpf -c biopattern.bpf.c -o biopattern.o
 bpftool gen skeleton biopattern.o > biopattern.skel.h
-gcc -O2 -g -o biopattern biopattern.c -lbpf -lelf -lz
+gcc -O2 -g -o biopattern biopattern.c trace_helpers.c -lbpf -lelf -lz
