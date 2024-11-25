98b125d66438083204e6f2b3aa1db79eb50e09d1
CAINIAOJL <1625602465@qq.com>
2024-11-25 22:32:13 +0800

    测试完成

--- Ebpf/lesson24-sudo/sudoadd.sh
+++ Ebpf/lesson24-sudo/sudoadd.sh
@@ -1,7 +1,7 @@
 #sudo ls /sys/kernel/debug/tracing/events/
 #sudo cat /sys/kernel/debug/tracing/events/syscalls/sys_enter_openat/format
 
-#bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux.h
+bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux.h
 clang -O2 -g -target bpf -c sudoadd.bpf.c -o sudoadd.o
 bpftool gen skeleton sudoadd.o > sudoadd.skel.h
 gcc -O2 -g -o sudoadd sudoadd.c -lbpf -lelf -lz
